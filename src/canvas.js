9d6163e83f0b6885f3067866196fa6b1fd13ba32
Louis Stowasser <Louis Stowasser@acer.(none)>
2010-12-22 12:33:22 +1000

    * Fixed attatch bug

--- src/canvas.js
+++ src/canvas.js
@@ -1,1 +1,131 @@
-/*** Canvas Components and Extensions*/Crafty.c("canvas", {	isCanvas: true,	buffer: 50,		init: function() {		//on change, redraw		this.bind("change", function(e) {			e = e || this;						//clear self			Crafty.context.clearRect(e._x, e._y, e._w, e._h);						//add to the DrawBuffer if visible			if((e._x + e._w > 0 - this.buffer && 			   e._y + e._h > 0 - this.buffer && 			   e._x < Crafty.viewport.width + this.buffer && 			   e._y < Crafty.viewport.height + this.buffer) ||			   			   (this._x + this._w > 0 - this.buffer && 			   this._y + this._h > 0 - this.buffer && 			   this._x < Crafty.viewport.width + this.buffer && 			   this._y < Crafty.viewport.height + this.buffer)) {			  				DrawBuffer.add(this,e);			}		});				this.bind("remove", function() {			//this.trigger("change");			Crafty.context.clearRect(this._x, this._y, this._w, this._h);			DrawBuffer.remove(this);		});	},		draw: function(x,y,w,h) {				var co = {}, //cached obj of position in sprite with offset			pos = { //inlined pos() function, for speed				_x: Math.floor(this._x),				_y: Math.floor(this._y),				_w: Math.floor(this._w),				_h: Math.floor(this._h)			},			coord = this.__coord || [];				//if offset		co.x = coord[0];		co.y = coord[1];		co.w = coord[2];		co.h = coord[3];				if(x !== undefined) {			co.x = coord[0] + x;			pos._x += x;						//if x is undefined, the rest of the arguments will be			if(y !== undefined) {				co.y = coord[1] + y;				pos._y += y;			}									if(w !== undefined) {				co.w = w;				pos._w = w;			}									if(h !== undefined) {				co.h = h;				pos._h = h;			}		}				this.trigger("draw",{type: "canvas", spritePos: co, pos: pos});				//inline drawing of the sprite		if(this.__c.sprite) {			//don't draw if not loaded			if(!this.img.width) return;						//draw the image on the canvas element			Crafty.context.drawImage(this.img, //image element									 co.x, //x position on sprite									 co.y, //y position on sprite									 co.w, //width on sprite									 co.h, //height on sprite									 pos._x, //x position on canvas									 pos._y, //y position on canvas									 pos._w, //width on canvas									 pos._h //height on canvas			);		}	}});Crafty.extend({	context: null,	_canvas: null,	gz: 0,		/**	* Set the canvas element and 2D context	*/	canvas: function(elem) {		//can pass a string with an ID		if(typeof elem === "string") {			elem = document.getElementById(elem);			//move node		} else if(!elem) {			elem = document.createElement("canvas");			this.stage.elem.appendChild(elem);		}				//check if is an actual canvas element		if(!('getContext' in elem)) {			Crafty.trigger("nocanvas");			return;		}		this.context = elem.getContext('2d');		this._canvas = elem;				//set canvas and viewport to the final dimensions		this._canvas.width = this.viewport.width;		this._canvas.height = this.viewport.height;	}});
+
+/**
+* Canvas Components and Extensions
+*/
+Crafty.c("canvas", {
+	isCanvas: true,
+	buffer: 50,
+	
+	init: function() {
+		//on change, redraw
+		this.bind("change", function(e) {
+			e = e || this;
+			
+			//clear self
+			Crafty.context.clearRect(e._x, e._y, e._w, e._h);
+			
+			//add to the DrawBuffer if visible
+			if((e._x + e._w > 0 - this.buffer && 
+			   e._y + e._h > 0 - this.buffer && 
+			   e._x < Crafty.viewport.width + this.buffer && 
+			   e._y < Crafty.viewport.height + this.buffer) ||
+			   
+			   (this._x + this._w > 0 - this.buffer && 
+			   this._y + this._h > 0 - this.buffer && 
+			   this._x < Crafty.viewport.width + this.buffer && 
+			   this._y < Crafty.viewport.height + this.buffer)) {
+			  
+				DrawBuffer.add(this,e);
+			}
+		});
+		
+		this.bind("remove", function() {
+			//this.trigger("change");
+			Crafty.context.clearRect(this._x, this._y, this._w, this._h);
+			DrawBuffer.remove(this);
+		});
+	},
+	
+	draw: function(x,y,w,h) {
+		
+		var co = {}, //cached obj of position in sprite with offset
+			pos = { //inlined pos() function, for speed
+				_x: Math.floor(this._x),
+				_y: Math.floor(this._y),
+				_w: Math.floor(this._w),
+				_h: Math.floor(this._h)
+			},
+			coord = this.__coord || [];
+		
+		//if offset
+		co.x = coord[0];
+		co.y = coord[1];
+		co.w = coord[2];
+		co.h = coord[3];
+		
+		if(x !== undefined) {
+			co.x = coord[0] + x;
+			pos._x += x;
+			
+			//if x is undefined, the rest of the arguments will be
+			if(y !== undefined) {
+				co.y = coord[1] + y;
+				pos._y += y;
+			}
+			
+			
+			if(w !== undefined) {
+				co.w = w;
+				pos._w = w;
+			}
+			
+			
+			if(h !== undefined) {
+				co.h = h;
+				pos._h = h;
+			}
+		}
+		
+		this.trigger("draw",{type: "canvas", spritePos: co, pos: pos});
+		
+		//inline drawing of the sprite
+		if(this.__c.sprite) {
+			//don't draw if not loaded
+			if(!this.img.width) return;
+			
+			//draw the image on the canvas element
+			Crafty.context.drawImage(this.img, //image element
+									 co.x, //x position on sprite
+									 co.y, //y position on sprite
+									 co.w, //width on sprite
+									 co.h, //height on sprite
+									 pos._x, //x position on canvas
+									 pos._y, //y position on canvas
+									 pos._w, //width on canvas
+									 pos._h //height on canvas
+			);
+		}
+	}
+});
+
+Crafty.extend({
+	context: null,
+	_canvas: null,
+	gz: 0,
+	
+	/**
+	* Set the canvas element and 2D context
+	*/
+	canvas: function(elem) {
+		//can pass a string with an ID
+		if(typeof elem === "string") {
+			elem = document.getElementById(elem);
+			//move node
+		} else if(!elem) {
+			elem = document.createElement("canvas");
+			this.stage.elem.appendChild(elem);
+		}
+		
+		//check if is an actual canvas element
+		if(!('getContext' in elem)) {
+			Crafty.trigger("nocanvas");
+			return;
+		}
+		this.context = elem.getContext('2d');
+		this._canvas = elem;
+		
+		//set canvas and viewport to the final dimensions
+		this._canvas.width = this.viewport.width;
+		this._canvas.height = this.viewport.height;
+	}
+});
--- src/drawing.js
+++ src/drawing.js
@@ -1,1 +1,101 @@
-Crafty.c("color", {	_color: "",		init: function() {		this.bind("draw", function(e) {			if(e.type === "DOM") {				e.style.background = this._color;				e.style.lineHeight = 0;			} else if(e.type === "canvas") {				if(this._color) Crafty.context.fillStyle = this._color;				Crafty.context.fillRect(e.pos._x,e.pos._y,e.pos._w,e.pos._h);			}		});	},		color: function(color) {		this._color = color;		this.trigger("change");		return this;	}});Crafty.c("image", {	_repeat: "repeat",		init: function() {		this.bind("draw", function(e) {			if(e.type === "canvas") {				this.canvasDraw();			} else if(e.type === "DOM") {				e.style.background = "url(" + this.__image + ") "+this._repeat;			}		});	},		image: function(url, repeat) {		this.__image = url;		this._repeat = repeat || "repeat";				if(this.has("canvas")) {			this.img = new Image();						this.img.src = url;						//draw when ready			Crafty.addEvent(this, this.img, 'load', function() {				DrawBuffer.add(this); //send to buffer to keep Z order			});		}		this.trigger("change");		return this;	},		canvasDraw: function() {		//skip if no image		if(!this.img) return;		var i = 0, l, j = 0, k;		switch(this._repeat) {			case "repeat-x":				if(this.img.width === 0) return;				for(l = Math.floor(this._w / this.img.width); i < l; i++) {					Crafty.context.drawImage(this.img, this._x + this.img.width * i, this._y);				}				break;			case "repeat-y":				if(this.img.height === 0) return;				for(l = Math.floor(this._h / this.img.height); i <= l; i++) {					Crafty.context.drawImage(this.img, this._x, this._y + this.img.height * i);				}				break;			default:				if(this.img.width === 0 || this.img.height === 0) return;				for(l = Math.floor(this._w / this.img.width); i < l; i++) {					Crafty.context.drawImage(this.img, this._x + this.img.width * i, this._y);					for(j = 0, k = Math.floor(this._h / this.img.height); j <= k; j++) {						Crafty.context.drawImage(this.img, this._x + this.img.width * i, this._y + this.img.height * j);					}				}								break;		}	}});Crafty.extend({	_scenes: [],	_current: null,		scene: function(name, fn) {		//play scene		if(arguments.length === 1) {			Crafty("2D").destroy(); //clear screen			this._scenes[name].call(this);			this._current = name;			return;		}		//add scene		this._scenes[name] = fn;		return;	}});
+Crafty.c("color", {
+	_color: "",
+	
+	init: function() {
+		this.bind("draw", function(e) {
+			if(e.type === "DOM") {
+				e.style.background = this._color;
+				e.style.lineHeight = 0;
+			} else if(e.type === "canvas") {
+				if(this._color) Crafty.context.fillStyle = this._color;
+				Crafty.context.fillRect(e.pos._x,e.pos._y,e.pos._w,e.pos._h);
+			}
+		});
+	},
+	
+	color: function(color) {
+		this._color = color;
+		this.trigger("change");
+		return this;
+	}
+});
+
+Crafty.c("image", {
+	_repeat: "repeat",
+	
+	init: function() {
+		this.bind("draw", function(e) {
+			if(e.type === "canvas") {
+				this.canvasDraw();
+			} else if(e.type === "DOM") {
+				e.style.background = "url(" + this.__image + ") "+this._repeat;
+			}
+		});
+	},
+	
+	image: function(url, repeat) {
+		this.__image = url;
+		this._repeat = repeat || "repeat";
+		
+		if(this.has("canvas")) {
+			this.img = new Image();
+			
+			this.img.src = url;
+			
+			//draw when ready
+			Crafty.addEvent(this, this.img, 'load', function() {
+				DrawBuffer.add(this); //send to buffer to keep Z order
+			});
+		}
+		this.trigger("change");
+		return this;
+	},
+	
+	canvasDraw: function() {
+		//skip if no image
+		if(!this.img) return;
+		var i = 0, l, j = 0, k;
+		switch(this._repeat) {
+			case "repeat-x":
+				if(this.img.width === 0) return;
+				for(l = Math.floor(this._w / this.img.width); i < l; i++) {
+					Crafty.context.drawImage(this.img, this._x + this.img.width * i, this._y);
+				}
+				break;
+			case "repeat-y":
+				if(this.img.height === 0) return;
+				for(l = Math.floor(this._h / this.img.height); i <= l; i++) {
+					Crafty.context.drawImage(this.img, this._x, this._y + this.img.height * i);
+				}
+				break;
+			default:
+				if(this.img.width === 0 || this.img.height === 0) return;
+				for(l = Math.floor(this._w / this.img.width); i < l; i++) {
+					Crafty.context.drawImage(this.img, this._x + this.img.width * i, this._y);
+					for(j = 0, k = Math.floor(this._h / this.img.height); j <= k; j++) {
+						Crafty.context.drawImage(this.img, this._x + this.img.width * i, this._y + this.img.height * j);
+					}
+				}
+				
+				break;
+		}
+	}
+});
+
+Crafty.extend({
+	_scenes: [],
+	_current: null,
+	
+	scene: function(name, fn) {
+		//play scene
+		if(arguments.length === 1) {
+			Crafty("2D").destroy(); //clear screen
+			this._scenes[name].call(this);
+			this._current = name;
+			return;
+		}
+		//add scene
+		this._scenes[name] = fn;
+		return;
+	}
+});
