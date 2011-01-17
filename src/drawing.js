8d0d1ed89546cc3750d3bdb790fc388c46072aac
Louis Stowasser <louisstow@gmail.com>
2011-01-17 15:18:13 +1000

    * Fixed background repeat for canvas

--- src/drawing.js
+++ src/drawing.js
@@ -26,9 +26,10 @@ Crafty.c("image", {
 	init: function() {
 		this.bind("draw", function(e) {
 			if(e.type === "canvas") {
-				this.canvasDraw();
+				this.canvasDraw(e);
 			} else if(e.type === "DOM") {
-				e.style.background = "url(" + this.__image + ") "+this._repeat;
+				if(this.__image) 
+					e.style.background = "url(" + this.__image + ") "+this._repeat;
 			}
 		});
 	},
@@ -39,45 +40,62 @@ Crafty.c("image", {
 		
 		if(this.has("canvas")) {
 			this.img = new Image();
-			
 			this.img.src = url;
 			
 			//draw when ready
-			Crafty.addEvent(this, this.img, 'load', function() {
-				DrawBuffer.add(this); //send to buffer to keep Z order
-			});
+			var self = this;
+			this.img.onload = function() {
+				DrawBuffer.add(self);
+			};
+		} else {
+			this.trigger("change");
 		}
-		this.trigger("change");
+		
 		return this;
 	},
 	
-	canvasDraw: function() {
+	canvasDraw: function(e) {
 		//skip if no image
 		if(!this.img) return;
 		
-		var i = 0, l, j = 0, k;
+		var i = 0, l, j, k, 
+			obj = e.pos || this,
+			xoffcut = this._w % this.img.width || this.img.width,
+			yoffcut = this._h % this.img.height || this.img.height,
+			width = this._w < this.img.width ? xoffcut : this.img.width,
+			height = this._h < this.img.height ? yoffcut : this.img.height;
 		
 		if(this._repeat === "no-repeat") {
-			
+			//draw once with no repeat
+			Crafty.context.drawImage(this.img, 0,0, width, height, obj._x, obj._y, width, height);
 		} else if(this._repeat === "repeat-x") {
-			if(this.img.width === 0) return;
-			
-			for(l = Math.floor(this._w / this.img.width); i < l; i++) {
-				Crafty.context.drawImage(this.img, this._x + this.img.width * i, this._y);
+			//repeat along the x axis
+			for(l = Math.ceil(this._w / this.img.width); i < l; i++) {
+				if(i === l-1) width = xoffcut;
+
+				Crafty.context.drawImage(this.img, 0, 0, width, height, obj._x + this.img.width * i, obj._y, width, height);
 			}
 		} else if(this._repeat === "repeat-y") {
-			if(this.img.height === 0) return;
-			
-			for(l = Math.floor(this._h / this.img.height); i <= l; i++) {
-				Crafty.context.drawImage(this.img, this._x, this._y + this.img.height * i);
+			//repeat along the y axis
+			for(l = Math.ceil(this._h / this.img.height); i < l; i++) {
+				//if the last image, determin how much to offcut
+				if(i === l-1) height = yoffcut;
+				
+				Crafty.context.drawImage(this.img, 0,0, width, height, obj._x, obj._y + this.img.height * i, width, height);
 			}
 		} else {
-			if(this.img.width === 0 || this.img.height === 0) return;
-			
-			for(l = Math.floor(this._w / this.img.width); i < l; i++) {
-				Crafty.context.drawImage(this.img, this._x + this.img.width * i, this._y);
-				for(j = 0, k = Math.floor(this._h / this.img.height); j <= k; j++) {
-					Crafty.context.drawImage(this.img, this._x + this.img.width * i, this._y + this.img.height * j);
+			//repeat all axis
+			for(l = Math.ceil(this._w / this.img.width); i < l; i++) {
+				if(i === l-1) width = xoffcut;
+				Crafty.context.drawImage(this.img, 0,0, width, height, obj._x + this.img.width * i, obj._y, width, height);
+				height = this._h < this.img.height ? yoffcut : this.img.height;
+				
+				for(j = 0, k = Math.ceil(this._h / this.img.height); j < k; j++) {
+					if(j === k-1) {
+						console.log(j , k);
+						height = yoffcut;
+					}
+					Crafty.context.drawImage(this.img, 0,0, width, height, obj._x + this.img.width * i, obj._y + this.img.height * j, width, height);
 				}
 			}
 		}
