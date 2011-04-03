1c3e73cd3c3011306e72b56f136b6b31a62c31a3
Leo Koppelkamm <diebuche@gmail.com>
2011-04-03 12:17:57 +0200

    Converting CRLF to LF.

--- build.php
+++ build.php
@@ -1,11 +1,12 @@
 <?php
+ini_set('auto_detect_line_endings',true);
 $src = "";
 $files = array("license.txt", "core.js","intro.js", "HashMap.js", "2D.js", "collision.js", "DOM.js","extensions.js","canvas.js","controls.js","animation.js",
 			   "drawing.js", "groups.js", "isometric.js", "particles.js", "sound.js", "text.js", "health.js", "scores.js", "loader.js", "outro.js");
 			   
 foreach($files as $file) {
 	$src .= file_get_contents("src/".$file);
-	$src .= "\n\r\n\r";
+	$src .= "\n\n";
 }
 
 file_put_contents("crafty.js", $src);
