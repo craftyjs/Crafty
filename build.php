b2004b37dde85339ad673a345dbe061da1b7bc65
Leo Koppelkamm <diebuche@gmail.com>
2011-04-07 00:13:19 +0200

    Readd build.php (or was this on purpose?)

---
+++ build.php
@@ -0,0 +1,15 @@
+<?php
+ini_set('auto_detect_line_endings',true);
+$src = "";
+$files = array("license.txt", "core.js","intro.js", "HashMap.js", "2D.js", "collision.js", "DOM.js","extensions.js","canvas.js","controls.js","animation.js",
+			   "drawing.js", "groups.js", "isometric.js", "particles.js", "sound.js", "text.js", "health.js", "scores.js", "loader.js", "outro.js");
+			   
+foreach($files as $file) {
+	$src .= file_get_contents("src/".$file);
+	$src .= "\n\n";
+}
+
+file_put_contents("crafty.js", $src);
+
+?>
+<h1>Done: Please YUI Compress!</h1>
