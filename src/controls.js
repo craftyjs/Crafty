25334a56bf253026c79a9fad58c5a77ca49ba2a1
sorenbs <sorenbs@gmail.com>
2011-05-15 20:45:08 +0200

    Closes #84 The more feature rich Multiway generalises the existing fourway and twoway components.

--- src/controls.js
+++ src/controls.js
@@ -297,6 +297,87 @@ Crafty.c("Keyboard", {
 });
 
 /**@
+* #Multiway
+* @category Input
+* Used to bind keys to directions and have the entity move acordingly 
+*/
+Crafty.c("Multiway", {	
+	_speed: 3,
+        
+	init: function() {
+            this._keyDirection = {};
+            this._keys = {};
+            this._movement= { x: 0, y: 0};
+	},
+	
+	/**@
+	* #.fourway
+	* @comp Multiway
+	* @sign public this .multiway([Number speed,] Object keyBindings )
+	* @param speed - Amount of pixels to move the entity whilst a key is down
+	* @param keyBindings - What keys should make the entity go in which direction. Direction is specified in degrees
+	* Constructor to initialize the speed and keyBindings. Component will listen for key events and move the entity appropriately. 
+	*
+	* When direction changes a NewDorection event is triggered with an object detailing the new direction: {x: x_movement, y: y_movement}
+	* When entity has moved on either x- or y-axis a Moved event is triggered with an object specifieng the old position {x: old_x, y: old_y}
+	* @example
+	* ~~~
+	* this.multiway(3, {UP_ARROW: -90, DOWN_ARROW: 90, RIGHT_ARROW: 0, LEFT_ARROW: 180});
+	* this.multiway({W: -90, S: 90, D: 0, A: 180});
+	* ~~~
+	*/
+	multiway: function(speed, keys) {
+		if(keys){
+                    this._speed = speed;
+                } else {
+                    keys = speed;
+                }
+                
+                this._keyDirection = keys;
+                this.speed(this._speed);
+
+                this.bind("KeyDown", function(e) {
+                    if(this._keys[e.key]) {
+                        this._movement.x = Math.round((this._movement.x + this._keys[e.keyCode].x)*1000)/1000;
+                        this._movement.y = Math.round((this._movement.y + this._keys[e.keyCode].y)*1000)/1000;
+                        this.trigger('NewDirection', this._movement);
+                    }
+                })
+                .bind("KeyUp", function(e) {
+                    if(this._keys[e.key]) {
+                        this._movement.x = Math.round((this._movement.x - this._keys[e.keyCode].x)*1000)/1000;
+                        this._movement.y = Math.round((this._movement.y - this._keys[e.keyCode].y)*1000)/1000;
+                        this.trigger('NewDirection', this._movement);
+                    }
+                })
+                .bind("enterframe",function() {
+                    if (this.disableControls) return;
+            
+                    if(this._movement.x !== 0) {
+                        this.x += this._movement.x;
+                        this.trigger('Moved', {x: this.x - this._movement.x, y: this.y});
+                    }
+                    if(this._movement.y !== 0) {
+                        this.y += this._movement.y;
+                        this.trigger('Moved', {x: this.x, y: this.y - this._movement.y});
+                    }
+		});
+		
+		return this;
+	},
+        
+        speed: function(speed) {
+                for(var k in this._keyDirection) {
+                   var keyCode = Crafty.keys[k] || k;
+                   this._keys[keyCode] = { x: Math.round(Math.cos(this._keyDirection[k]*(Math.PI/180))*1000 * speed)/1000,
+                                           y: Math.round(Math.sin(this._keyDirection[k]*(Math.PI/180))*1000 * speed)/1000}
+                }
+
+                return this;
+        }
+});
+
+/**@
 * #Fourway
 * @category Input
 * Move an entity in four directions by using the
