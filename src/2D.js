craftyjs/Crafty

commit d93e87d412cb929ba41cc88c28a129bdc4e3a363

Use DOM event properties to get mouse event targets for DOM entities.

src/controls.js
@@ -17,38 +17,47 @@ Crafty.extend({
 			i = 0, l,
 			pos = Crafty.DOM.translate(e.clientX, e.clientY),
 			x, y,
-			dupes = {};
+			dupes = {},
+			tar = e.target?e.target:e.srcElement,
+			ent = Crafty(parseInt(tar.id.replace('ent', '')));
 		
 		e.realX = x = pos.x;
 		e.realY = y = pos.y;
 		
-		//search for all mouse entities
-		q = Crafty.map.search({_x: x, _y:y, _w:1, _h:1}, false);
-		
-		for(l=q.length;i<l;++i) {
-			//check if has mouse component
-			if(!q[i].__c.Mouse) continue;
+		if (tar.nodeName != "CANVAS") {
+			// we clicked on a dom element
+			if (ent.has('Mouse'))
+				closest = ent;
+		}
+		else {
+			//search for all mouse entities
+			q = Crafty.map.search({_x: x, _y:y, _w:1, _h:1}, false);
 			
-			var current = q[i],
-				flag = false;
+			for(l=q.length;i<l;++i) {
+				//check if has mouse component
+				if(!q[i].__c.Mouse) continue;
 				
-			//weed out duplicates
-			if(dupes[current[0]]) continue;
-			else dupes[current[0]] = true;
-			
-			if(current.map) {
-				if(current.map.containsPoint(x, y)) {
-					flag = true;
+				var current = q[i],
+					flag = false;
+					
+				//weed out duplicates
+				if(dupes[current[0]]) continue;
+				else dupes[current[0]] = true;
+				
+				if(current.map) {
+					if(current.map.containsPoint(x, y)) {
+						flag = true;
+					}
+				} else if(current.isAt(x, y)) flag = true;
+				
+				if(flag && (current._z >= maxz || maxz === -1)) {
+					//if the Z is the same, select the closest GUID
+					if(current._z === maxz && current[0] < closest[0]) {
+						continue;
+					}
+					maxz = current._z;
+					closest = current;
 				}
-			} else if(current.isAt(x, y)) flag = true;
-			
-			if(flag && (current._z >= maxz || maxz === -1)) {
-				//if the Z is the same, select the closest GUID
-				if(current._z === maxz && current[0] < closest[0]) {
-					continue;
-				}
-				maxz = current._z;
-				closest = current;
 			}
 		}
 		

crafty.js
@@ -3558,38 +3558,47 @@ Crafty.extend({
 			i = 0, l,
 			pos = Crafty.DOM.translate(e.clientX, e.clientY),
 			x, y,
-			dupes = {};
+			dupes = {},
+			tar = e.target?e.target:e.srcElement,
+			ent = Crafty(parseInt(tar.id.replace('ent', '')));
 		
 		e.realX = x = pos.x;
 		e.realY = y = pos.y;
 		
+		if (tar.nodeName != "CANVAS") {
+			// we clicked on a dom element
+			if (ent.has('Mouse'))
+				closest = ent;
+		}
+		else {
 		//search for all mouse entities
-		q = Crafty.map.search({_x: x, _y:y, _w:1, _h:1}, false);
-		
-		for(l=q.length;i<l;++i) {
-			//check if has mouse component
-			if(!q[i].__c.Mouse) continue;
+			q = Crafty.map.search({_x: x, _y:y, _w:1, _h:1}, false);
 			
-			var current = q[i],
-				flag = false;
+			for(l=q.length;i<l;++i) {
+				//check if has mouse component
+				if(!q[i].__c.Mouse) continue;
 				
-			//weed out duplicates
-			if(dupes[current[0]]) continue;
-			else dupes[current[0]] = true;
-			
-			if(current.map) {
-				if(current.map.containsPoint(x, y)) {
-					flag = true;
+				var current = q[i],
+					flag = false;
+					
+				//weed out duplicates
+				if(dupes[current[0]]) continue;
+				else dupes[current[0]] = true;
+				
+				if(current.map) {
+					if(current.map.containsPoint(x, y)) {
+						flag = true;
+					}
+				} else if(current.isAt(x, y)) flag = true;
+				
+				if(flag && (current._z >= maxz || maxz === -1)) {
+					//if the Z is the same, select the closest GUID
+					if(current._z === maxz && current[0] < closest[0]) {
+						continue;
+					}
+					maxz = current._z;
+					closest = current;
 				}
-			} else if(current.isAt(x, y)) flag = true;
-			
-			if(flag && (current._z >= maxz || maxz === -1)) {
-				//if the Z is the same, select the closest GUID
-				if(current._z === maxz && current[0] < closest[0]) {
-					continue;
-				}
-				maxz = current._z;
-				closest = current;
 			}
 		}
 		

commit b6694af580fcae0bf14d2efbfb7c6f14ccb23e66

when determining which entity the mouse is over take the map set by areamap into account. The behavior was changed in d93e87d412cb929ba41cc88c28a129bdc4e3a363 which caused isometric games to malfunction. This fixes 115 (well, the issue raised in the coment of that ticket)

src/2D.js
@@ -385,6 +385,9 @@ Crafty.c("2D", {
 	* an object can't be passed. The arguments require the x and y value
 	*/
 	isAt: function(x,y) {
+		if(this.map) {
+			return this.map.containsPoint(x,y);
+		}
 		return this.x <= x && this.x + this.w >= x &&
 			   this.y <= y && this.y + this.h >= y;
 	},

src/controls.js
@@ -32,10 +32,10 @@ Crafty.extend({
 				tar = tar.parentNode;
 			}
 			ent = Crafty(parseInt(tar.id.replace('ent', '')))
-			if (ent.has('Mouse'))
+			if (ent.has('Mouse') && ent.isAt(x,y))
 				closest = ent;
 		}
-		else {
+		if(!closest) {
 			//search for all mouse entities
 			q = Crafty.map.search({_x: x, _y:y, _w:1, _h:1}, false);
 			