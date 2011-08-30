c8b067f9d0f271c6f991791e7a8ab9989c842c60
Louis Stowasser <louisstow@gmail.com>
2011-08-31 09:39:01 +1000

    * Build and API gen

---
+++ build/footer.php
@@ -0,0 +1,29 @@
+</div>
+</div>
+
+<div class="clearer"></div>
+<div id="footer">
+
+<div id="contact">
+	<a href="http://twitter.com/craftyjs"><img src="../images/twitter.png"/> @craftyjs</a>
+	<a href="http://webchat.freenode.net"><img src="../images/comment.png"/> #crafty@freenode.net</a>
+	<a href="http://groups.google.com/group/craftyjs"><img src="../images/google.png"/> google groups</a>
+	<a href="mailto:hello@craftyjs.com"><img src="../images/email.png"/> hello@craftyjs.com</a>
+	<a href="https://github.com/louisstow/Crafty"><img src="../images/github.png"/> github</a>
+</div>
+
+<ul>
+	<li><a href="../api/">Documentation</a></li>
+	<li><a href="../tutorials.php">Tutorial</a></li>
+	<li><a href="../demos.php">Demos</a></li>
+	<!-- <li><a href="#comingsoon">Builder<sup>new</sup></a></li> -->
+	<li><a href="../forums/">Forums</a></li>
+	<li><a href="http://craftyjs.tumblr.com/">Blog</a></li>
+	<!-- <li><a href="api">FAQ</a></li>-->
+	<li class="emph"><a href="../download.php">Download</a></li>
+</ul>
+<p>&copy; Crafty 2010. Crafty is distributed under the <a href="http://en.wikipedia.org/wiki/MIT_License">MIT</a> or 
+<a href="http://en.wikipedia.org/wiki/GNU_General_Public_License">GPL</a> license.</p>
+</div>
+</body>
+</html>
