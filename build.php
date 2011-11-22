8d5e6b15cab1be47f82105e7e4c6e98a52bea69b
sorenbs <sorenbs@gmail.com>
2011-11-21 21:19:16 -0800

    Add math.js

--- build.php
+++ build.php
@@ -2,7 +2,7 @@
 ini_set('auto_detect_line_endings',true);
 $src = "";
 $files = array("license.txt", "core.js","intro.js", "HashMap.js","2D.js", "collision.js", "DOM.js","extensions.js","canvas.js","controls.js","animation.js","sprite.js",
-			   "drawing.js", "isometric.js", "particles.js", "sound.js","html.js", "text.js", "loader.js", "outro.js");
+			   "drawing.js", "isometric.js", "particles.js", "sound.js","html.js", "text.js", "loader.js", "math.js", "outro.js");
 			   
 foreach($files as $file) {
 	$src .= file_get_contents("src/".$file);
