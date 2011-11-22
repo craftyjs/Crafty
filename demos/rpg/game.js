29b918bdb46ddaff0c3273d22ef0aff0ffe4b972
sorenbs <sorenbs@gmail.com>
2011-11-21 21:44:24 -0800

    update for 0.4.5

--- demos/rpg/game.js
+++ demos/rpg/game.js
@@ -21,12 +21,12 @@ window.onload = function() {
 		for(var i = 0; i < 25; i++) {
 			//generate the grass along the y-axis
 			for(var j = 0; j < 20; j++) {
-				grassType = Crafty.randRange(1, 4);
+				grassType = Crafty.math.randomInt(1, 4);
 				Crafty.e("2D, Canvas, grass"+grassType)
 					.attr({x: i * 16, y: j * 16});
 				
 				//1/50 chance of drawing a flower and only within the bushes
-				if(i > 0 && i < 24 && j > 0 && j < 19 && Crafty.randRange(0, 50) > 49) {
+				if(i > 0 && i < 24 && j > 0 && j < 19 && Crafty.math.randomInt(0, 50) > 49) {
 					Crafty.e("2D, DOM, flower, solid, SpriteAnimation")
 						.attr({x: i * 16, y: j * 16})
 						.animate("wind", 0, 1, 3)
@@ -40,18 +40,18 @@ window.onload = function() {
 		
 		//create the bushes along the x-axis which will form the boundaries
 		for(var i = 0; i < 25; i++) {
-			Crafty.e("2D, Canvas, wall_top, solid, bush"+Crafty.randRange(1,2))
+			Crafty.e("2D, Canvas, wall_top, solid, bush"+Crafty.math.randomInt(1,2))
 				.attr({x: i * 16, y: 0, z: 2});
-			Crafty.e("2D, DOM, wall_bottom, solid, bush"+Crafty.randRange(1,2))
+			Crafty.e("2D, DOM, wall_bottom, solid, bush"+Crafty.math.randomInt(1,2))
 				.attr({x: i * 16, y: 304, z: 2});
 		}
 		
 		//create the bushes along the y-axis
 		//we need to start one more and one less to not overlap the previous bushes
 		for(var i = 1; i < 19; i++) {
-			Crafty.e("2D, DOM, wall_left, solid, bush"+Crafty.randRange(1,2))
+			Crafty.e("2D, DOM, wall_left, solid, bush"+Crafty.math.randomInt(1,2))
 				.attr({x: 0, y: i * 16, z: 2});
-			Crafty.e("2D, Canvas, wall_right, solid, bush"+Crafty.randRange(1,2))
+			Crafty.e("2D, Canvas, wall_right, solid, bush"+Crafty.math.randomInt(1,2))
 				.attr({x: 384, y: i * 16, z: 2});
 		}
 	}
