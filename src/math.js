8f7a327cb5f82080775ed28846b663961fba3eec
Philipp Zins <philippzins@googlemail.com>
2011-11-22 09:53:51 +0100

    Added Vector2D class to Crafty.math.

--- src/math.js
+++ src/math.js
@@ -1,3 +1,7 @@
+/**
+ * Crafty.math - Static functions.
+ */
+
 Crafty.math = {
     /**
      * Returns the absolute value.
@@ -146,5 +150,220 @@ Crafty.math = {
     withinRange: function(value, min, max) {
         return (value >= min && value <= max);
     }
+};
 
+/**
+ * Crafty.math - Classes.
+ */
+
+/**
+ * Vector2D
+ *
+ * A vector class for basic vector calculations.
+ * @param x The x value of the vector (default: 0).
+ * @param y The y value of the vector (default: 0).
+ */
+Crafty.math.Vector2D = (function() {
+    // place vor private attributes/methodes
+
+    // return constructor
+    return function Vector2D(x, y) {
+        if (arguments.length === 1)
+            y = 0;
+        else if (arguments.length === 0)
+            x = y = 0;
+
+        this.x = x;
+        this.y = y;
+    };
+})();
+
+/**
+ * Public functions.
+ */
+
+/**
+ * Normalize vector. (Set length to 1.)
+ * @return This vector.
+ */
+Crafty.math.Vector2D.prototype.normalize = function() {
+    var l = this.getLength();
+    if (l === 0)
+        this.x = 1;
+    else {
+        this.x = this.x / l;
+        this.y = this.y / l;
+    }
+    return this;
+};
+
+/**
+ * Get vector length.
+ * @return Length of Vector.
+ */
+Crafty.math.Vector2D.prototype.getLength = function() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+};
+
+/**
+ * Set vector length.
+ * @param length New length of Vector.
+ */
+Crafty.math.Vector2D.prototype.setLength = function(length) {
+    this.normalize().multiply(length);
+};
+
+/**
+ * Calculate the dot product of this vector and another.
+ * @param vector Another vector.
+ * @return The dot product.
+ */
+Crafty.math.Vector2D.prototype.dotProduct = function(vector) {
+    return this.x * vector.x + this.y * vector.y;
+};
+
+/**
+ * Calculate normal of line between this vector and another and
+ * returns righthand normal vector (-dy, dx).
+ * (Lefthand would be (dy, -dx). dy first because normal is rotated in 90° to vector.)
+ * @param vector Another vector. If no vector is passed, a zero-vector will be used.
+ * @return Normal vector.
+ */
+Crafty.math.Vector2D.prototype.getNormal = function(vector) {
+    if (arguments.length === 0) vector = new Crafty.math.Vector2D();
+    return new Crafty.math.Vector2D(-(this.y - vector.y), this.x - vector.x);	// (-dy, dx) == right or (dy, -dx) == left
+};
+
+/**
+ * Get angle between this vector and another in rad.
+ * Math.acos( a * b / ( |a| * |b| ) );
+ * @param vector Another vector.
+ * @return The angle between this vector and another in rad.
+ */
+Crafty.math.Vector2D.prototype.getAngle = function(vector) {
+    return Math.acos(this.dotProduct(vector) / (this.getLength() * vector.getLength()));
+    // or: Math.atan2(y, x) ?
+};
+
+/**
+ * Checks if x and y are 0.
+ * @return A boolean which specifies if this vector is a zero-vector.
+ */
+Crafty.math.Vector2D.prototype.isZero = function() {
+    return this.x === 0 && this.y === 0;
+};
+
+/**
+ * Negates this vector (-x, -y).
+ * @return Returns this vector.
+ */
+Crafty.math.Vector2D.prototype.negate = function() {
+    this.x *= -1;
+    this.y *= -1;
+    return this;
+};
+
+/**
+ * Calculates distance between this vector and another.
+ * @param vector Another vector.
+ * @return Returns distance.
+ */
+Crafty.math.Vector2D.prototype.distance = function(vector) {
+    return Math.sqrt(this.squaredDistance(vector));
+};
+
+/**
+ * Multiply this vector with a scalar.
+ * @param scalar A scalar which will be multiplied to this vector.
+ * @return This vector.
+ */
+Crafty.math.Vector2D.prototype.multiply = function(scalar) {
+    this.x *= scalar;
+    this.y *= scalar;
+    return this;
+};
+
+/**
+ * Calculates squared distance between this vector and another.
+ * @param vector Another vector.
+ * @return Returns squared distance.
+ */
+Crafty.math.Vector2D.prototype.squaredDistance = function(vector) {
+    return (this.x - vector.x) * (this.x - vector.x) + (this.y - vector.y) * (this.y - vector.y);
+};
+
+/**
+ * Subtracts another vector from this vector.
+ * @param vector Another vector.
+ * @return This vector.
+ */
+Crafty.math.Vector2D.prototype.subtract = function(vector) {
+    this.x -= vector.x;
+    this.y -= vector.y;
+    return this;
+};
+
+/**
+ * Adds another vector to this vector.
+ * @param vector Another vector.
+ * @return This vector.
+ */
+Crafty.math.Vector2D.prototype.add = function(vector) {
+    this.x += vector.x;
+    this.y += vector.y;
+    return this;
+};
+
+/**
+ * Creates a copy of this vector.
+ * @return Copy of this vector.
+ */
+Crafty.math.Vector2D.prototype.getCopy = function() {
+    return new Crafty.math.Vector2D(this.x, this.y);
+};
+
+/**
+ * Creates a new vector wich is the negative vector of this one.
+ * @return New vector, which is the negative of this one.
+ */
+Crafty.math.Vector2D.prototype.getNegative = function() {
+    return new Crafty.math.Vector2D(-this.x, -this.y);
+};
+
+/**
+ * Get new vector from this vector to another.
+ * @param vector Another vector.
+ * @return New vector which points form this vector to antoher..
+ */
+Crafty.math.Vector2D.prototype.to = function(vector) {
+    return new Crafty.math.Vector2D(vector.x - this.x, vector.y - this.y);
+};
+
+/**
+ * Set x and y values of this vector
+ * @param vector Holds the new x and y values.
+ */
+Crafty.math.Vector2D.prototype.setValues = function(vector) {
+    this.x = vector.x;
+    this.y = vector.y;
+};
+
+/**
+ * Static public functions.
+ */
+
+/**
+ * Create a triple product.
+ * @param a Vecvtor1.
+ * @param b Vecvtor2.
+ * @param c Vecvtor3.
+ * @return Returns the triple product of three vectors as a new vector.
+ */
+Crafty.math.Vector2D.tripleProduct = function(a, b, c) {
+    var v = new Crafty.math.Vector2D();
+    var ac = a.dotProduct(c);
+    var bc = b.dotProduct(c);
+    v.x = b.x * ac - a.x * bc;
+    v.y = b.y * ac - a.y * bc;
+    return v;
 };
