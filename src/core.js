3e4f84422cb049001b7dbea54591ac4cc6277949
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2012-02-12 11:50:38 +0100

    rename delay into timeout

--- src/core.js
+++ src/core.js
@@ -371,9 +371,9 @@
 		},
 
 		/**@
-	* #.delay
+	* #.timeout
 	* @comp Crafty Core
-	* @sign public this .delay(Function callback, Number delay)
+	* @sign public this .timeout(Function callback, Number delay)
 	* @param callback - Method to execute after given amount of milliseconds
 	* @param delay - Amount of milliseconds to execute the method
 	* The delay method will execute a function after a given amount of time in milliseconds.
@@ -383,12 +383,12 @@
 	* @example
     * Destroy itself after 100 milliseconds
 	* ~~~
-	* this.delay(function() {
+	* this.timeout(function() {
 	     this.destroy();
 	* }, 100);
 	* ~~~
 	*/
-		delay: function (fn, duration) {
+		timeout: function (fn, duration) {
 			this.each(function () {
 				var self = this;
 				setTimeout(function () {
