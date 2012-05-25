f80542b0a3fa3039ed8454d870ca2fee0a95a820
Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
2012-05-25 14:35:44 +0200

    dont fire StopDrag for all entity after first mouseup

--- src/controls.js
+++ src/controls.js
@@ -389,10 +389,12 @@ Crafty.c("Draggable", {
 		};
 
 		this._onup = function upper(e) {
-			Crafty.removeEvent(this, Crafty.stage.elem, "mousemove", this._ondrag);
-			Crafty.removeEvent(this, Crafty.stage.elem, "mouseup", this._onup);
-			this._dragging = false;
-			this.trigger("StopDrag", e);
+            if (this._dragging == true) {
+                Crafty.removeEvent(this, Crafty.stage.elem, "mousemove", this._ondrag);
+			    Crafty.removeEvent(this, Crafty.stage.elem, "mouseup", this._onup);
+			    this._dragging = false;
+    			this.trigger("StopDrag", e);
+            }
 		};
 
 		this.enableDrag();
