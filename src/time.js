8f4a899c94ccc5ada9c0712444ec47319fdb1b8c
mucaho <mkucko@gmail.com>
2013-03-26 18:49:09 +0100

    Update time.js
    Added changes as suggested by https://github.com/craftyjs/Crafty/pull/396

--- src/time.js
+++ src/time.js
@@ -12,8 +12,17 @@ Crafty.c("Delay", {
 				var item = this._delays[index];
 				if(item.start + item.delay + item.pause < now) {
 					item.func.call(this);
-					// remove item from array
-					this._delays.splice(index,1);
+					if (item.repeat > 0 ) {
+						// reschedule item
+						item.start = now;
+						item.pause = 0;
+						item.pauseBuffer = 0;
+						if (item.repeat !== Infinity)
+							item.repeat--;
+					} else if (item.repeat === 0) {
+						// remove item from array
+						this._delays.splice(index,1);
+					}
 				}
 			}
 		});
@@ -37,6 +46,9 @@ Crafty.c("Delay", {
 	* @sign public this.delay(Function callback, Number delay)
 	* @param callback - Method to execute after given amount of milliseconds
 	* @param delay - Amount of milliseconds to execute the method
+	* @param repeat - How often to repeat the delayed function. A value of 0 triggers the delayed
+	* function exactly once. A value n > 0 triggers the delayed function exactly n+1 times. A
+	* value of Infinity triggers the delayed function indefinitely.
 	* 
 	* The delay method will execute a function after a given amount of time in milliseconds.
 	* 
@@ -51,16 +63,18 @@ Crafty.c("Delay", {
 	* console.log("start");
 	* this.delay(function() {
 		 console.log("100ms later");
-	* }, 100);
+	* }, 100, 0);
 	* ~~~
 	*/
-	delay : function(func, delay) {
-		return this._delays.push({
+	delay : function(func, delay, repeat) {
+		this._delays.push({
 			start : new Date().getTime(),
 			func : func,
 			delay : delay,
+			repeat: repeat || 0,
 			pauseBuffer: 0,
 			pause: 0
 		});
+		return this;
 	}
 });
