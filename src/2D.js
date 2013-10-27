b175f340dc52cf43579bbe3a76f3e389ab75c861
Tim Martin <starwed@gmail.com>
2013-10-27 11:19:13 -0400

    Remove vestigal component 'Physics'

--- src/2D.js
+++ src/2D.js
@@ -1009,16 +1009,6 @@ Crafty.c("2D", {
     }
 });
 
-Crafty.c("Physics", {
-    _gravity: 0.4,
-    _friction: 0.2,
-    _bounce: 0.5,
-
-    gravity: function (gravity) {
-        this._gravity = gravity;
-    }
-});
-
 /**@
  * #Gravity
  * @category 2D
