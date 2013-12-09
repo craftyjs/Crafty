f505850e06eeb5bf13bba7545b6b6123cead76f8
pawurb <p.urbanek89@gmail.com>
2013-12-09 18:54:49 +0100

    Added checking trailing spaces to jshint.

--- Gruntfile.js
+++ Gruntfile.js
@@ -78,6 +78,7 @@ module.exports = function (grunt) {
         jshint: {
             files: ['Gruntfile.js', 'src/**/*.js'],
             options: {
+                trailing: true,
                 globals: {
                 }
             }
