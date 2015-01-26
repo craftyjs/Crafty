0ac4877348137b97c67298bdcc8245302c986ec5
Jeremy Neiman <docmarionum1@gmail.com>
2015-01-26 18:29:48 -0500

    Testing stopPropagation/preventDefault on keyboard events.

---
+++ tests/controls.js
@@ -0,0 +1,29 @@
+(function() {
+    module("Controls");
+    
+    test("stopKeyPropagation", function() {
+      var stopPropCalled = false;
+      var preventDefaultCalled = false;
+
+      var mockEvent = {
+        char:"", charCode:"", keyCode:"", type:"", 
+        shiftKey:"", ctrlKey:"", metaKey:"", timestamp:"",
+        target: Crafty.canvas._canvas,
+        stopPropagation: function(){
+          stopPropCalled = true;
+        },
+        preventDefault: function(){
+          preventDefaultCalled = true;
+        },
+        cancelBubble: false,
+        returnValue: false,
+      };
+
+      Crafty.selected = true;
+      Crafty.keyboardDispatch(mockEvent);
+      Crafty.selected = false;
+      
+      ok(stopPropCalled, "stopPropagation Not Called");
+      ok(preventDefaultCalled, "preventDefault Not Called");
+    });
+})();
