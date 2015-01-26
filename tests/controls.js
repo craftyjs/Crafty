6eb1af365bd6ff4d41dc5507c076af07bf814560
Jeremy Neiman <docmarionum1@gmail.com>
2015-01-26 12:15:50 -0500

    Preventing default keyboard actions needs to be on original
    stopPropagation, cancelBubble, etc. need to be used in the original event, not the Crafty copy of it, e.
    
    Adds tests for this behavior.

--- tests/controls.js
+++ tests/controls.js
@@ -69,4 +69,29 @@
       equal(touchEndsOverEntities, 2, "Two entities recieved TouchEnd");
     });
     
+    test("stopKeyPropagation", function() {
+      var stopPropCalled = false;
+      var preventDefaultCalled = false;
+
+      var mockEvent = {
+        char:"", charCode:"", keyCode:"", type:"", 
+        shiftKey:"", ctrlKey:"", metaKey:"", timestamp:"",
+        target: document,
+        stopPropagation: function(){
+          stopPropCalled = true;
+        },
+        preventDefault: function(){
+          preventDefaultCalled = true;
+        },
+        cancelBubble: false,
+        returnValue: false,
+      };
+
+      Crafty.selected = true;
+      Crafty.keyboardDispatch(mockEvent);
+      Crafty.selected = false;
+      
+      ok(stopPropCalled, "stopPropagation Not Called");
+      ok(preventDefaultCalled, "preventDefault Not Called");
+    });
 })();
