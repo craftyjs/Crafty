dafeb19c59ff5e4b184ea2adc8d0396d19414b1a
mucaho <mkucko@gmail.com>
2015-07-16 11:13:34 +0200

    Extract logging tests into new file.

--- tests/debug.js
+++ tests/debug.js
@@ -1,31 +1,6 @@
 (function() {
   var module = QUnit.module;
 
-  module("Crafty.log");
-
-  test("Logging works when console.log is enabled", function(){
-    var original_log = console.log;
-    var logged_message = "";
-    console.log = function(msg) { logged_message = msg; };
-    var test_message = "test message";
-    
-    Crafty.log(test_message);
-    equal(logged_message, test_message, "Crafty.log correctly passes through to console.log");
-    
-    Crafty.loggingEnabled = false;
-    logged_message = "";
-    Crafty.log(test_message);
-    equal(logged_message, "", "Crafty.log does nothing when logging is disabled.");
-    Crafty.loggingEnabled = true;
-
-    console.log = undefined;
-    Crafty.log(test_message);
-    equal(logged_message, "", "Crafty.log does not crash when console.log is undefined.");
-
-
-    console.log = original_log;
-  });
-
   module("DebugLayer");
 
   test("DebugCanvas", function() {
