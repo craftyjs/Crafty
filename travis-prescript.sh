d8ce0913b5c39276ada9936b6ebf0e795a7ade11
Tim Martin <starwed@gmail.com>
2016-11-27 14:35:10 -0500

    More fixables

--- travis-prescript.sh
+++ travis-prescript.sh
@@ -1,5 +1,6 @@
-if [ "$TRAVIS_BRANCH" != "testing" ]; then unset SAUCE_USERNAME && unset SAUCE_ACCESS_KEY; fi
+npm install -g grunt-cli
 
 # Work around for phantomjs+yarn install bug
 # See this issue for info: https://github.com/yarnpkg/yarn/issues/1538
 TMPDIR= $npm_config_tmp
+export TMPDIR
