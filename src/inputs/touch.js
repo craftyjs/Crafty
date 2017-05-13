c289eda7c360b75ac7f77b5a98c6455522464331
mucaho <mucaho@gmail.com>
2017-05-13 11:22:10 +0200

    Change TouchSystem
    Change TouchSystem to inherit functionality from TouchState.
    **Breaking change:**
    * TouchStart and TouchEnd are no longer triggered
    when a finger enters or leaves entity. They are now triggered only
    once, when a finger is pressed or raised on the touch surface, no
    matter which entity was targeted.
    * TouchOver and TouchOut replace those events, which are triggered
    when a finger enters or leaves the entity.
    
    Add alias for Crafty.touchHandler.fingers.
    
    Fix lifecycle onBlur events.

--- src/inputs/touch.js
+++ src/inputs/touch.js
@@ -5,42 +5,44 @@ Crafty.extend({
      * #Crafty.multitouch
      * @category Input
      * @kind Method
+     *
+     * Enables/disables support for multitouch feature.
      * @sign public this .multitouch(Boolean bool)
      * @param bool - Turns multitouch on and off.  The initial state is off (false).
      *
+     * Query whether multitouch is on or off.
      * @sign public Boolean .multitouch()
-     * @returns Whether multitouch is currently enabled;
+     * @returns Whether multitouch is currently enabled
      *
-     * Enables/disables support for multitouch feature.
+     * By default, touch events are treated as mouse events and are handled by the `MouseSystem`.
+     * To change this behaviour and handle events by the `TouchSystem`, enable multitouch.
      *
-     * If this is set to true, it is expected that your entities have the Touch component instead of Mouse component.
+     * If this is set to true, it is expected that your entities have the `Touch` component instead of the `Mouse` component.
      * If false (default), then only entities with the Mouse component will respond to touch.
+     * It's recommended to add the `Button` component instead, which requires the proper component depending on this feature.
      *
-     * If no boolean is passed to the function call, it will just return whether multitouch is on or not.
-     *
-     * @note The Touch component (and thus the multitouch feature) is currently incompatible with the Draggable component.
+     * @note The multitouch feature is currently incompatible with the `Draggable` component and `Crafty.viewport.mouselook`.
      *
      * @example
      * ~~~
      * Crafty.multitouch(true);
+     * Crafty.log("multitouch is " + Crafty.multitouch());
      *
-     * var myEntity1 = Crafty.e('2D, Canvas, Color, Touch')
-     *    .attr({x: 100, y: 100, w:200, h:200, z:1 })
+     * Crafty.e('2D, Canvas, Color, Button')
+     *    .attr({ x: 100, y: 100, w:200, h:200, z:1 })
      *    .color('black')
-     *    .bind('TouchStart',function(e){ alert('big black box was touched', e); }),
-     *  myEntity2 = Crafty.e('2D, Canvas, Color, Touch')
-     *    .attr({x: 40, y: 150, w:90, h:300, z:2 })
-     *    .color('green')
-     *    .bind('TouchStart',function(e){ alert('big GREEN box was touched', e); });
-     *
-     * Crafty.log("multitouch is "+Crafty.multitouch());
+     *    .bind('TouchStart', function(e) { this.color('green'); });
      * ~~~
      * @see TouchSystem
+     * @see MouseSystem
+     * @see Button
      * @see Touch
+     * @see Mouse
      */
     multitouch: function (bool) {
         if (typeof bool !== "boolean") return this._multitouch;
         this._multitouch = bool;
+        return this;
     },
      _multitouch: false,
 
@@ -115,188 +117,182 @@ Crafty.extend({
  * @category Input
  * @kind System
  *
- * Provides access to touch events.
+ * Provides access to touch point events.
+ * @note Additional events and methods are inherited from the `TouchState` component.
  *
- * This system dispatches touch events received by Crafty (crafty.stage.elem).
- * The touch events get dispatched to the closest entity to the source of the event (if available).
+ * @trigger TouchOver - when a finger enters an entity - TouchPointEvent
+ * @trigger TouchOut - when a finger leaves an entity - TouchPointEvent
  *
- * By default, touch events are treated as mouse events. To change this behaviour (and enable multitouch)
- * you must use Crafty.multitouch.
+ * The event callbacks are triggered with the native [`TouchEvent`](https://developer.mozilla.org/en-US/docs/Web/API/TouchEvent)
+ * received by Crafty's stage (`Crafty.stage.elem`), which is wrapped in a standard Crafty event object (see `TouchState`).
+ * Note that for each changed [`Touch` point](https://developer.mozilla.org/en-US/docs/Web/API/Touch)) a separate
+ * Crafty TouchPointEvent event is triggered.
  *
- * If using multitouch feature, this method sets the array Crafty.touchHandler.fingers, which holds data
- * of the most recent touches that occured (useful for determining positions of fingers in every frame)
- * as well as last entity touched by each finger. Data is lost as soon as the finger is raised.
+ * These touch point events are triggered on the TouchSystem itself.
+ * Additionally, they are dispatched to the closest (visible & `Touch`-enhanced) entity to the source of the event (if available).
  *
- * You can read about the MouseEvent, which is the parameter passed to the Mouse entity's callback.
- * https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent
+ * @note By default, touch events are treated as mouse events and are not triggered by this system.
+ * To change this behaviour (and enable multitouch) use `Crafty.multitouch`.
  *
- * You can also read about the TouchEvent.
- * https://developer.mozilla.org/en-US/docs/Web/API/TouchEvent
- *
- * And about the touch point interface, which is the parameter passed to the Touch entity's callback.
- * http://www.w3.org/TR/touch-events/#dfn-active-touch-point
- *
+ * @see TouchState, Touch
  * @see Crafty.multitouch
- * @see Touch
  */
-Crafty.s("Touch", Crafty.extend.call(new Crafty.__eventDispatcher(), {
+Crafty.s("Touch", Crafty.extend.call(Crafty.extend.call(new Crafty.__eventDispatcher(), {
+    normedEventNames: {
+        "touchstart": "TouchStart",
+        "touchmove": "TouchMove",
+        "touchend": "TouchEnd",
+        "touchcancel": "TouchCancel" // touchcancel is treated as touchend, but triggers a TouchCancel event
+    },
+
+    _evt: { // evt object to reuse
+        eventName:'',
+        identifier: -1,
+        target: null,
+        entity: null, // DEPRECATED: remove this in upcoming release
+        realX: 0,
+        realY: 0,
+        originalEvent: null
+    },
+
     // Indicates how many entities have the Touch component, for performance optimization
+    // Touch events are still routed to Crafty.s('Touch') even if there are no entities with Touch component
     touchObjs: 0,
 
-    // TODO: move routing of events in future to controls system
-    // and change it so that touch events are always triggered on TouchSystem
-    dispatchEvent: function (e) {
-        if (!this.touchObjs) return;
+    // current entites that are pointed at
+    overs: {},
 
-        switch (e.type) {
-            case "touchstart":
-                this._touchHandler.handleStart(e);
-                break;
-            case "touchmove":
-                this._touchHandler.handleMove(e);
-                break;
-            case "touchleave": // touchleave is treated as touchend
-            case "touchcancel": // touchcancel is treated as touchend, but triggers a TouchCancel event
-            case "touchend":
-                this._touchHandler.handleEnd(e);
-                break;
+    prepareEvent: function (e, type) {
+        var evt = this._evt;
+
+        // Normalize event name
+        evt.eventName = this.normedEventNames[type] || type;
+
+        // copy identifier
+        evt.identifier = e.identifier;
+
+        // augment touch event with real coordinates
+        Crafty.translatePointerEventCoordinates(e, evt);
+
+        // augment touch event with target entity
+        evt.target = this.touchObjs ? Crafty.findPointerEventTargetByComponent("Touch", e) : null;
+        // DEPRECATED: remove this in upcoming release
+        evt.entity = evt.target;
+
+        return evt;
+    },
+
+    // this method will be called by TouchState iff triggerTouch event was valid
+    triggerTouchEvent: function(eventName, e) {
+        // trigger event on TouchSystem itself
+        this.trigger(eventName, e);
+
+        var identifier = e.identifier,
+            closest = e.target,
+            over = this.overs[identifier];
+
+        if (over) { // if old TouchOver target wasn't null, send TouchOut
+            if ((eventName === "TouchMove" && over !== closest) || // if TouchOver target changed
+                eventName === "TouchEnd" || eventName === "TouchCancel") { // or TouchEnd occurred
+
+                e.eventName = "TouchOut";
+                e.target = over;
+                e.entity = over; // DEPRECATED: remove this in upcoming release
+                over.trigger("TouchOut", e);
+                e.eventName = eventName;
+                e.target = closest;
+                e.entity = closest; // DEPRECATED: remove this in upcoming release
+
+                // delete old over entity
+                delete this.overs[identifier];
+            }
+        }
+
+        // TODO: move routing of events in future to controls system, make it similar to KeyboardSystem
+        // try to find closest element that will also receive touch event, whatever the event is
+        if (closest) {
+            closest.trigger(eventName, e);
+        }
+
+        if (closest) { // if new TouchOver target isn't null, send TouchOver
+            if (eventName === "TouchStart" || // if TouchStart occurred
+                (eventName === "TouchMove" && over !== closest)) { // or TouchOver target changed
+
+                e.eventName = "TouchOver";
+                closest.trigger("TouchOver", e);
+                e.eventName = eventName;
+
+                // save new over entity
+                this.overs[identifier] = closest;
+            }
         }
     },
 
-    _touchHandler: {
-        fingers: [], // keeps track of touching fingers
-
-        handleStart: function (e) {
-            var touches = e.changedTouches;
-            for (var i = 0, l = touches.length; i < l; i++) {
-                var idx = false,
-                    closest;
-                closest = this.findClosestTouchEntity(touches[i]);
-
-                if (closest) {
-                    closest.trigger("TouchStart", touches[i]);
-                    // In case the entity was already being pressed, get the finger index
-                    idx = this.fingerDownIndexByEntity(closest);
-                }
-                var touch = this.setTouch(touches[i], closest);
-                if (idx !== false && idx >= 0) {
-                    // Recycling finger...
-                    this.fingers[idx] = touch;
-                } else {
-                    this.fingers.push(touch);
-                }
-            }
-        },
-
-        handleMove: function (e) {
-            var touches = e.changedTouches;
-            for (var i = 0, l = touches.length; i < l; i++) {
-                var idx = this.fingerDownIndexById(touches[i].identifier);
-                var closest = this.findClosestTouchEntity(touches[i]);
-
-                if (idx >= 0) {
-                    var finger = this.fingers[idx];
-                    if(typeof finger.entity !== "undefined")
-                        if (finger.entity === closest) {
-                            finger.entity.trigger("TouchMove", touches[i]);
-                        } else {
-                            if (typeof closest === "object") closest.trigger("TouchStart", touches[i]);
-                            finger.entity.trigger("TouchEnd");
-                        }
-                    finger.entity = closest;
-                    finger.realX = touches[i].realX;
-                    finger.realY = touches[i].realY;
-                }
-            }
-        },
-
-        handleEnd: function (e) {
-            var touches = e.changedTouches,
-                eventName = e.type === "touchcancel" ? "TouchCancel" : "TouchEnd";
-            for (var i = 0, l = touches.length; i < l; i++) {
-                var idx = this.fingerDownIndexById(touches[i].identifier);
-
-                if (idx >= 0) {
-                    if (this.fingers[idx].entity)
-                        this.fingers[idx].entity.trigger(eventName);
-                    this.fingers.splice(idx, 1);
-                }
-            }
-        },
-
-        setTouch: function (touch, entity) {
-            return { identifier: touch.identifier, realX: touch.realX, realY: touch.realY, entity: entity };
-        },
-
-        findClosestTouchEntity: function (touchEvent) {
-            Crafty.augmentPointerEvent(touchEvent);
-            return Crafty.findPointerEventTargetByComponent("Touch", touchEvent);
-        },
-
-        fingerDownIndexById: function (idToFind) {
-            for (var i = 0, l = this.fingers.length; i < l; i++) {
-                var id = this.fingers[i].identifier;
-                if (id === idToFind) {
-                    return i;
-                }
-            }
-            return -1;
-        },
-
-        fingerDownIndexByEntity: function (entityToFind) {
-            for (var i = 0, l = this.fingers.length; i < l; i++) {
-                var ent = this.fingers[i].entity;
-
-                if (ent === entityToFind) {
-                    return i;
-                }
-            }
-            return -1;
+    dispatchEvent: function (e) {
+        var evt, touches = e.changedTouches;
+        for (var i = 0, l = touches.length; i < l; i++) {
+            evt = this.prepareEvent(touches[i], e.type);
+            // wrap original event into standard Crafty event object
+            evt.originalEvent = e;
+            this.triggerTouch(evt.eventName, evt);
         }
     }
-}), {}, false);
+}), Crafty.__touchStateTemplate), {}, false);
 
 /**@
  * #Touch
  * @category Input
  * @kind Component
  *
- * Provides the entity with touch related events
+ * Provides the entity with touch point events.
+ * Touch point events get dispatched to the closest (visible & `Touch`-enhanced) entity to the source of the event (if available).
  * @note If you do not add this component, touch events will not be triggered on the entity.
  *
- * @trigger TouchStart - when entity is touched - TouchPoint
- * @trigger TouchMove - when finger is moved over entity - TouchPoint
- * @trigger TouchCancel - when a touch event has been disrupted in some way - TouchPoint
- * @trigger TouchEnd - when the finger is raised over the entity, or when finger leaves entity.  (Passes no data) - null
+ * Triggers all events described in `TouchSystem` and `TouchState`, these are:
+ * @trigger TouchOver - when a finger enters the entity - TouchPointEvent
+ * @trigger TouchMove - when a finger is over the entity and moves - TouchPointEvent
+ * @trigger TouchOut - when a finger leaves the entity - TouchPointEvent
+ * @trigger TouchStart - when a finger is pressed on the entity - TouchPointEvent
+ * @trigger TouchEnd - when a finger is raised over the entity - TouchPointEvent
+ * @trigger TouchCancel - when a touch event has been disrupted in some way whilst over the entity - TouchPointEvent
  *
- * To be able to use multitouch, you must enable it with  `Crafty.multitouch(true)`.
- *
- * If you don't need multitouch, you can probably use the Mouse component instead, since by default Crafty will trigger mouse events for touch input.
- *
- * You can read more about the TouchEvent.
- * - [TouchEvent.touches and TouchEvent.changedTouches](https://developer.mozilla.org/en-US/docs/Web/API/TouchEvent)
- * - [TouchPoint](http://www.w3.org/TR/touch-events/#dfn-active-touch-point) is the parameter passed to the event callback in the related touch.
- *
- * The passed TouchPoints are augmented by properties which correspond to the coordinates of the TouchEvent in world (default viewport) space,
- * namely `TouchPoint.realX` and `TouchPoint.realY`.
+ * @note By default, touch events are treated as mouse events and are not triggered by this component.
+ * To change this behaviour (and enable multitouch) use `Crafty.multitouch`.
  *
  * @example
  * ~~~
  * Crafty.multitouch(true);
  *
- * var myEntity = Crafty.e('2D, Canvas, Color, Touch')
- * .attr({x: 10, y: 10, w: 40, h: 40})
- * .color('green')
- * .bind('TouchStart', function(TouchPoint){
- *   Crafty.log('myEntity has been touched', TouchPoint);
- * }).bind('TouchMove', function(TouchPoint) {
- *   Crafty.log('Finger moved over myEntity at the { x: ' + TouchPoint.realX + ', y: ' + TouchPoint.realY + ' } coordinates.');
- * }).bind('TouchEnd', function() {
- *   Crafty.log('Touch over myEntity has finished.');
- * });
+ * Crafty.e('2D, Canvas, Color, Touch')
+ *   .attr({x: 10, y: 10, w: 40, h: 40})
+ *   .color('green')
+ *   .bind('TouchOver', function(TouchPoint){
+ *     Crafty.log('A finger is over the entity', TouchPoint.identifier);
+ *   })
+ *   .bind('TouchMove', function(TouchPoint) {
+ *     Crafty.log('A finger moves over the entity at { x: ' + TouchPoint.realX + ', y: ' + TouchPoint.realY + ' } coordinates.');
+ *   })
+ *   .bind('TouchOut', function(TouchPoint){
+ *     Crafty.log('A finger is no longer over the entity', TouchPoint.identifier);
+ *   });
  * ~~~
+ *
+ * @example
+ * ~~~
+ * Crafty.multitouch(true);
+ *
+ * var myEntity1 = Crafty.e('2D, Canvas, Color, Touch')
+ *    .attr({x: 100, y: 100, w:200, h:200, z:1 })
+ *    .color('black')
+ *    .bind('TouchStart',function(e){ alert('big black box was touched', e); }),
+ *  myEntity2 = Crafty.e('2D, Canvas, Color, Touch')
+ *    .attr({x: 40, y: 150, w:90, h:300, z:2 })
+ *    .color('green')
+ *    .bind('TouchStart',function(e){ alert('big green box was touched', e); });
+ * ~~~
+ *
+ * @see TouchState, TouchSystem
  * @see Crafty.multitouch
- * @see TouchSystem
  */
 Crafty.c("Touch", {
     required: "AreaMap",
